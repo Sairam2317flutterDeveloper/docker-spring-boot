a20899aac8f673a8055bc0fe57e402913088845f
Sairam_Software_Enginner <saisofttech2317@gmail.com>
2026-04-15 23:29:50 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Apr 15th, 2026");
-        model.addAttribute("msg", "Hello All.. My Self Sairam We are deploying springboot Microservice App into EKS cluster using Helm + Jenkins Pipeline!!!!");
+        model.addAttribute("msg", "Hello All..We are deploying springboot Microservice App into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
 
